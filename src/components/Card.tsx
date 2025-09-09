5555f6c992d1fa3e5308a91eccd87946038c9735
TanishDadheech <tdadheech07@gmail.com>
2025-09-09 23:49:21 +0530

    Updated card component.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
 export function Card(){
     return <div  className="py-8 bg-white rounded-md border border-gray-200 max-w-96">
         Hi There!!!
+        This is a card component!
     </div>
 }
